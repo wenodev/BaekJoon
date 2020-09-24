641c651f220001e1a3d4669b10892d6e0b6969e3
logicstark <logicalstark1105@gmail.com>
2020-09-24 11:00:46 +0900

    fix : Scanner대신 BufferedReader사용

--- src/ch04/수정렬하기_TEST.java
+++ src/ch04/수정렬하기_TEST.java
@@ -1,28 +1,39 @@
 package ch04;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.TreeSet;
 
+
+
+/*
+
+1. 입력을 받을때는 BufferedReader를 사용하는 습관을 들이자
+2. 정렬을 할때는 TreeSet보단 List형태로 받은 뒤 Collectinons.sort() 를 사용하는 것이 더 효율적이다.
+
+ */
+
+
 public class 수정렬하기_TEST {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
+    public static void main(String[] args) throws IOException {
 
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
         Set<Integer> set = new TreeSet<>();
 
         for (int i=0; i<count; i++){
-            int temp = sc.nextInt();
+            int temp = Integer.parseInt(br.readLine());
             set.add(temp);
         }
 
         set.forEach(n->{
             System.out.print(n + " ");
         });
-
 
     }
 

9df489c63bbf82e1e23a7c0bef7b9c86e50fd26f
logicstark <logicalstark1105@gmail.com>
2020-11-23 20:33:07 +0900

    [create] 선택정렬

--- src/weno/algorithm/Selection.java
+++ src/weno/algorithm/Selection.java
@@ -1,19 +1,12 @@
 package weno.algorithm;
 
-public class Insertion {
-
-
-
-
+public class Selection {
 
     public static void insertion_sort(int[] data){
-
         int temp;
-
         for (int i = 0; i < data.length-1; i++) {
 
             for (int j = i+1; j < data.length; j++) {
-
                 if (data[i] > data[j]){
                     temp = data[i];
                     data[i] = data[j];
@@ -21,20 +14,15 @@ public class Insertion {
                 }
             }
         }
-
     }
 
 
     public static void main(String[] args) {
-
         int[] data = {8, 5, 6, 2, 4};
         insertion_sort(data);
 
         for (int i = 0; i < data.length; i++) {
             System.out.print(data[i] + " ");
         }
-
     }
-
-
 }

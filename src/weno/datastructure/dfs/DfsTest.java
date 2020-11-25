aa9c267e6b450e82075d496ebd8ec0c1efbe3437
wenodev <logicalstark1105@gmail.com>
2020-11-25 12:45:29 +0900

    [create] DFS

--- src/weno/datastructure/dfs/DfsTest.java
+++ src/weno/datastructure/dfs/DfsTest.java
@@ -4,5 +4,18 @@ public class DfsTest {
 
     public static void main(String[] args) {
 
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.DFS(0); /* 주어진 노드를 시작 노드로 DFS 탐색 */
+
     }
+
+
 }
--- src/weno/datastructure/dfs/Graph.java
+++ src/weno/datastructure/dfs/Graph.java
@@ -1,4 +1,44 @@
 package weno.datastructure.dfs;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Graph {
+
+    public int SIZE;
+    public LinkedList<Integer> adj[];
+
+    public Graph(int size){
+        this.SIZE = size;
+        adj = new LinkedList[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int x, int y){
+        adj[x].add(y);
+    }
+
+    public void DFSUtil(int data, boolean visited[]){
+
+        visited[data] = true;
+        System.out.print(data + " ");
+
+        Iterator<Integer> iterator = adj[data].listIterator();
+        while(iterator.hasNext()){
+            int num = iterator.next();
+
+            if (!visited[num]){
+                DFSUtil(num, visited);
+            }
+        }
+    }
+
+    public void DFS(int data){
+        boolean visited[] = new boolean[SIZE];
+        DFSUtil(data, visited);
+    }
+
 }

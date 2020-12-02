df65d09e6ac85d965cf59b477ec007cd22988180
wenodev <logicalstark1105@gmail.com>
2020-12-02 17:56:39 +0900

    [update] 팩토리얼 & 피보나치

---
+++ src/weno/basic/recursive/Factorial.java
@@ -0,0 +1,21 @@
+package weno.basic.recursive;
+
+public class Factorial {
+
+    public static int factorial(int number){
+
+        if (number == 1)
+            return 1;
+        else{
+            return number*factorial(number-1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial(3));
+
+    }
+
+}
---
+++ src/weno/basic/recursive/Fibonacci.java
@@ -0,0 +1,15 @@
+package weno.basic.recursive;
+
+public class Fibonacci {
+
+    public static int fibonacci(int number){
+        if (number == 1) return 1;
+        else if (number == 2) return 1;
+        else return fibonacci(number-2) + fibonacci(number-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(4));
+    }
+
+}
